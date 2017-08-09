8a31f6bce33efb67e73e0ab4e05b368b9dedbb4c
vitalets <vitalets@yandex-team.ru>
2017-08-09 17:50:14 +0300

    update deps

--- test/globals.js
+++ test/globals.js
@@ -6,4 +6,8 @@ const chaiAsPromised = require('chai-as-promised');
 chai.use(chaiAsPromised);
 
 global.assert = chai.assert;
+global.expect = chai.expect;
 global.srcPath = process.env.TEST_LIB ? '../../lib' : '../../src';
+
+// see: https://github.com/domenic/chai-as-promised/issues/173
+process.on('unhandledRejection', () => {});
